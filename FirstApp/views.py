03981a0b10c25b8ad64e41ef1fb2da54c79c5a9e
dzd07 <proko.piotr@gmail.com>
2021-11-15 17:08:53 +0100

    wsio

--- FirstApp/views.py
+++ FirstApp/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
+from django.forms import ImgForm
 
+
+class Image(TemplateView):
+    form = ImgForm
+    template_name = 'FirstApp/image.html'
+
+    def post(self, request, *args, **kwargs):
+        form = ImgForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            return HttpResponseRedirect(reverse_lazy('image_display', kwargs={'pk': obj.id}))
+
+        context = self.get_contaxt_data(form=form)
+        return self.render_to_response(context)
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class ImageDisplay(DetailView):
+    model = Post
+    template_name = 'FirstApp/image_display.html'
+    context_object_name = 'image'
 # Create your views here.
 
 
