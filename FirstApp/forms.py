48d3b4f9f2e59f52e9aa9ac6065d909e7f8ebc72
dzd07 <proko.piotr@gmail.com>
2021-11-18 12:04:05 +0100

    ostatnie zmiany, edit post

--- FirstApp/forms.py
+++ FirstApp/forms.py
@@ -3,6 +3,9 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+
+    title = forms.CharField(help_text='Max 200 znaków')
+
     class Meta:
         model = Post
         fields = ['title', 'text', 'image']
